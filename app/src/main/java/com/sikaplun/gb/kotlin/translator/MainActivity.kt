a129ac646610b0cc00ee5964f838a1cabf348d3a
sergiikaplun <sikaplun@gmail.com>
2022-03-31 06:52:32 +0300

    Initial commit

---
+++ app/src/main/java/com/sikaplun/gb/kotlin/translator/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sikaplun.gb.kotlin.translator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
